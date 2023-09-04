bcb6d3985ef5a20918216075bac5578b2f0ccf94
syan797 <syan797@aucklanduni.ac.nz>
2023-09-04 19:34:26 +1200

    step2 type info functionality

--- web-assignment-pokemon/script.js
+++ web-assignment-pokemon/script.js
@@ -10,10 +10,17 @@ window.addEventListener("load", function(){
         let pokemonTable = document.querySelector("#allPokemonTable");
         
         for (let i = 0; i < pokemonArray.length; i++) {
-            let pokemonName = pokemonArray[i].name;
+            //Creating a new row & cell
             let newRow = document.createElement("tr");
             let newCell = document.createElement("td");
-            newCell.innerText = pokemonName;
+            
+            //Setting up new cell with appropriate name and event listener
+            let pokemon = pokemonArray[i];
+            newCell.innerText = pokemon.name;
+            newCell.setAttribute("data-dex-num", pokemon.dexNumber);
+            newCell.addEventListener("click", displaySpecificPokemonDetails);
+
+            //Adding the new row & cell to the table
             pokemonTable.appendChild(newRow);
             newRow.appendChild(newCell);
         }
@@ -31,12 +38,24 @@ window.addEventListener("load", function(){
         displayPokemonDetails(randomPokemonObj);
     }
 
+    async function displaySpecificPokemonDetails(event) {
+        let dexNum = event.target.getAttribute("data-dex-num");
+        let pokemonResponse = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${dexNum}`);
+        let pokemonObj = await pokemonResponse.json();
+        displayPokemonDetails(pokemonObj);
+    }
+
     function displayPokemonDetails(pokemonObj) {
         insertName(pokemonObj.name);
         insertImage(pokemonObj.imageUrl, pokemonObj.name);
         insertPokedexNum(pokemonObj.dexNumber);
         insertDescription(pokemonObj.dexEntry);
-        insertTypesList(pokemonObj.types);
+        
+        let typesArray = pokemonObj.types;
+        insertTypesList(typesArray);
+        typesArray.forEach((type) => addOffenseTable(type));
+
+        addDefenseTable(pokemonObj.dexNumber);
     }
 
     function insertName(name) {
@@ -64,11 +83,74 @@ window.addEventListener("load", function(){
 
     function insertTypesList(typesArray) {
         let typesList = document.querySelector("#typesList");
+        
         for (let i = 0; i < typesArray.length; i++) {
-            typesList.innerHTML += typesArray[i];
+            type = typesArray[i];
+            typesList.innerHTML += type;
             if (i != typesArray.length-1) {
                 typesList.innerHTML += ", ";
             }
+        }
+    }
+
+    async function addOffenseTable(type) {
+        //Setting up the table
+        let offenseTables = document.querySelector("#offenseTablesContainer");
+        let table = document.createElement("table");
+        table.classList.add("typeTable");
+        table.innerHTML =   `<thead>
+                                <tr>
+                                    <th colspan="2">${type} type attacks:</th>
+                                </tr>
+                                <tr>
+                                    <th>Defending type</th><th>Damage dealt</th>
+                                </tr>
+                            </thead>`;
+        offenseTables.appendChild(table);
+        let tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+
+        //Retrieving the offense data
+        let typeResponse = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/types/${type}`);
+        let typeObj = await typeResponse.json();
+        let offenseArray = typeObj.offenseDamageMultipliers;
+
+        //Adding the data to the table
+        offenseArray.forEach((rowInfo) => addRowInfo(rowInfo, tbody));
+    }
+
+    async function addDefenseTable(dexNum) {
+        //Getting the tbody of the table
+        let tbody = document.querySelector("#defenseTBody");
+
+        //Retrieving the defense data
+        let defenseResponse = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${dexNum}/defense-profile`);
+        let defenseArray = await defenseResponse.json();
+        
+        //Adding the data to the table
+        defenseArray.forEach((rowInfo) => addRowInfo(rowInfo, tbody));
+    }
+
+    function addRowInfo(rowInfo, tbody) {
+        let newRow = document.createElement("tr");
+        let multiplierText = getMultiplierText(rowInfo.multiplier);
+        newRow.innerHTML = `<td>${rowInfo.type}</td><td>${multiplierText}</td>`;
+        tbody.appendChild(newRow);
+    }
+
+    function getMultiplierText(num) {
+        if (num == 0) {
+            return "No damage";
+        } else if (num == 0.25) {
+            return "Quarter damage";
+        } else if (num == 0.5) {
+            return "Half damage";
+        } else if (num == 1) {
+            return "Normal damage";
+        } else if (num == 2) {
+            return "Double damage";
+        } else if (num == 4) {
+            return "Quadruple damage";
         }
     }
 
