0e92e12f513c32609ec8bf45bafd352a74e9a300
syan797 <syan797@aucklanduni.ac.nz>
2023-09-04 15:16:26 +1200

    step2 loading all pokemon

--- web-assignment-pokemon/script.js
+++ web-assignment-pokemon/script.js
@@ -1,7 +1,27 @@
 window.addEventListener("load", function(){
 
     // Your client-side JavaScript here
+    displayAllPokemon();
 
     // All data from the server must be accessed via AJAX fetch requests.
+    async function displayAllPokemon() {
+        let pokemonArray = await getPokemonArray();
+        let pokemonTable = document.querySelector("#allPokemonTable");
+        
+        for (let i = 0; i < pokemonArray.length; i++) {
+            let pokemonName = pokemonArray[i].name;
+            let newRow = document.createElement("tr");
+            let newCell = document.createElement("td");
+            newCell.innerText = pokemonName;
+            pokemonTable.appendChild(newRow);
+            newRow.appendChild(newCell);
+        }
+    }
+
+    async function getPokemonArray() {
+        let allPokemonResponse = await fetch("https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon");
+        let allPokemonArray = await allPokemonResponse.json();
+        return allPokemonArray;
+    }
 
 });
